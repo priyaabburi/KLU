daa18753873c5c0e96f4bc8932712a8d44bdf1ad
sree padmapriya abburi <2000030018@kluniversity.in>
2022-12-27 09:21:21 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatabindComponent } from './databind/databind.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'data',
+    component:DatabindComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'gallery',
+    component:GalleryComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:ErrorpageComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { DatabindComponent } from './databind/databind.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    GalleryComponent,
+    ErrorpageComponent,
+    DatabindComponent
   ],
   imports: [
     BrowserModule,
